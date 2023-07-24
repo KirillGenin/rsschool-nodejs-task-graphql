6130907dc5cc708720088502d9ae8911863f9b09
KirillGenin <332211op@gmail.com>
2023-07-25 01:34:30 +0300

    feat: add validation graphql query

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, validate, parse } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -14,6 +15,12 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query: source, variables: variableValues } = req.body;
+
+      const errors = validate(schema, parse(source), [depthLimit(5)]);
+
+      if (errors && errors.length > 0) {
+        return { data: '', errors };
+      }
 
       const res = await graphql({
         schema,
