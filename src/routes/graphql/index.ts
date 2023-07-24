d187ee19ad22fe69a6adb9ddea99323458345bea
KirillGenin <332211op@gmail.com>
2023-07-25 00:04:28 +0300

    chore: add basic graphql schema

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,5 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
 import { graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
@@ -13,7 +13,17 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const { query: source, variables: variableValues } = req.body;
+
+      const res = await graphql({
+        schema,
+        source,
+        variableValues,
+      });
+
+      const { data } = res;
+
+      return { data };
     },
   });
 };
